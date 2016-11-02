255e40618f322d7b2a121abddf899956d3fa492f
CraigLangford <craigllangford@gmail.com>
2016-11-02 16:59:32 +0000

    Functional test now checks if we can input a to-do item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -3,6 +3,7 @@ Following Book
 '''
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase): 
@@ -21,17 +22,32 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header.text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         
         # She types "Buy cake" into a text box (she loves cake)
+        inputbox.send_keys('Buy cake')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy cake" as an item in a to-do list
+        inputbox.send_keys('Keys.Enter')
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy cake' for row in rows)
+        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Eat cake" (Mudi is very methodical)
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
