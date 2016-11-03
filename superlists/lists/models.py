ff164fd48db5a907b3b77484a5ac2b7b24abbda1
CraigLangford <craigllangford@gmail.com>
2016-11-03 15:19:19 +0000

    Created a list object which contains items.

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
-class Item(models.Model):
+class List(models.Model):
     text = models.TextField(default='')
     pass
 
-
+class Item(models.Model):
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default='')
